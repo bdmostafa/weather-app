36b4ec7363d41c7de93cb8b944a62f497781f96c
Mostafa <mdmostafaalmahmud@gmail.com>
2020-08-15 22:20:58 +0600

    icon added and getTime getDate function added in UI class

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ class Weather {
     }
     async getWeather() {
         try {
-            const data = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.country}&APPID=${this.appId}&units=metric`);
+            const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.country}&APPID=${this.appId}&units=metric`);
             const response = await data.json();
 
             console.log(response);
-            // Return the essential data as an object from the whole response data 
+            // Return the essential data as an object data from the whole response data 
             return {
                 location_data: [response.name, response.sys.country],
                 cloudiness_data: response.clouds.all,
@@ -25,15 +25,6 @@ class Weather {
         }
 
     }
-    getTime() {
-        const date = new Date();
-        return `${date.toLocaleDateString()}  ${date.toLocaleTimeString()}`;
-    }
-    getDay() {
-        const day = new Date();
-        let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-        return `${days[day.getDay()]}`;
-    }
     setLocation(city, country) {
         this.city = city;
         this.country = country;
@@ -43,7 +34,7 @@ class Weather {
 // Instantiate weather class
 const weather = new Weather('Dhaka', 'BD', apiId)
 // weather.setLocation('Los Angeles', 'US')
-// weather.setLocation('Edmonton', 'CA')
+weather.setLocation('Edmonton', 'CA')
 
 
 // Handle UI of Weather App =================================
@@ -68,7 +59,7 @@ class UI {
 
     // Print all the data on UI section of Weather App
     // paint(day, time, weather){}
-    paint(day, time, {
+    paint({
         // weather object destructuring - receiving as parameter from data argument
         location_data,
         cloudiness_data,
@@ -79,22 +70,45 @@ class UI {
             humidity,
             pressure
         },
+        weather_data: {
+            description,
+            icon
+        },
         sun_data,
-        weather_data,
         wind_data
     }) {
-        this.dayToday.textContent = day;
-        this.dateTime.textContent = time;
+        // Create icon url to add into src attribute of icon img
+        const iconUrl = UI.generateIcon(icon);
+        this.mainForecastIcon.setAttribute('src', iconUrl);
+        // Get local weak day
+        this.dayToday.textContent = UI.getDay();
+        // Get local date and time
+        this.dateTime.textContent = UI.getTime();
+        // Location - city and country
         this.location.textContent = `${location_data[0]}, ${location_data[1]}`;
-        this.weatherDescription.textContent = weather_data.description;
+        this.weatherDescription.textContent = description;
         this.feelings.innerHTML = `${feels_like}<sup>o</sup>C`;
         this.cloudiness.textContent = `${cloudiness_data}%`;
-        this.temperature.textContent = temp;
+        this.temperature.innerHTML = `${temp}<sup>o</sup>C`;
         this.wind.textContent = ` ${wind_data}m/s`;
         this.humidity.textContent = `${humidity}%`;
         this.pressure.textContent = pressure;
         this.sunrise.textContent = (new Date(sun_data.sunrise)).toLocaleTimeString();
         this.sunset.textContent = (new Date(sun_data.sunset)).toLocaleTimeString();
+    }
+    static getTime() {
+        const date = new Date();
+        return `${date.toLocaleDateString()}  ${date.toLocaleTimeString()}`;
+    }
+    static getDay() {
+        const day = new Date();
+        let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        return `${days[day.getDay()]}`;
+    }
+    // Icon generate
+    static generateIcon(icon) {
+        // console.log(icon)
+        return `http://openweathermap.org/img/w/${icon}.png`;
     }
 }
 
@@ -103,9 +117,6 @@ const ui = new UI();
 
 
 weather.getWeather().then(data => {
-    const day = weather.getDay();
-    const time = weather.getTime();
-    // three arguments passing day, time and data object
-    ui.paint(day, time, data);
+    ui.paint(data);
     console.log(data)
 })
