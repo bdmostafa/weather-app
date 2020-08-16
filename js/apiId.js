79df519e9c1afd23cec1e87851aaaca2903e0578
Mostafa <mdmostafaalmahmud@gmail.com>
2020-08-16 19:55:59 +0600

    apiId.js file removed from gitignore

---
+++ js/apiId.js
@@ -0,0 +1,14 @@
+// API Weather
+// https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=c26c6010e343573d2391962f7361a387&units=metric
+
+// API Forecast
+// https://api.openweathermap.org/data/2.5/forecast/daily?q={city name}&cnt={cnt}&appid={your api key}
+
+
+
+const apiId = 'c26c6010e343573d2391962f7361a387';
+export {
+    apiId
+};
+
+// module.exports = 'c26c6010e343573d2391962f7361a387';
